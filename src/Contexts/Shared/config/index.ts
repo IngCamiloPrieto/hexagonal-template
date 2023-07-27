b2e263587a058fa1adf53b1aa6444745f3c1b883
Cristian Paez <cristianp@keoworld.com>
2023-07-27 15:36:12 -0500

    config files by environment

---
+++ src/Contexts/Shared/config/index.ts
@@ -0,0 +1,130 @@
+import convict from 'convict';
+
+const customerConfig = convict({
+  env: {
+    doc: 'The application environment.',
+    format: ['production', 'development', 'staging', 'test'],
+    default: 'default',
+    env: 'NODE_ENV'
+  },
+  mongo: {
+    url: {
+      doc: 'The Mongo connection URL',
+      format: String,
+      env: 'MONGO_URL',
+      default: 'mongodb://localhost:27017/mooc-backend-dev'
+    }
+  },
+  typeorm: {
+    host: {
+      doc: 'The database host',
+      format: String,
+      env: 'TYPEORM_HOST',
+      default: 'localhost'
+    },
+    port: {
+      doc: 'The database port',
+      format: Number,
+      env: 'TYPEORM_PORT',
+      default: 5432
+    },
+    username: {
+      doc: 'The database username',
+      format: String,
+      env: 'TYPEORM_USERNAME',
+      default: 'keo'
+    },
+    password: {
+      doc: 'The database password',
+      format: String,
+      env: 'TYPEORM_PASSWORD',
+      default: 'K30!'
+    },
+    database: {
+      doc: 'The database name',
+      format: String,
+      env: 'TYPEORM_DATABASE',
+      default: 'keoworld'
+    }
+  },
+  rabbitmq: {
+    connectionSettings: {
+      username: {
+        doc: 'RabbitMQ username',
+        format: String,
+        env: 'RABBITMQ_USERNAME',
+        default: 'guest'
+      },
+      password: {
+        doc: 'RabbitMQ password',
+        format: String,
+        env: 'RABBITMQ_PASSWORD',
+        default: 'guest'
+      },
+      vhost: {
+        doc: 'RabbitMQ virtual host',
+        format: String,
+        env: 'RABBITMQ_VHOST',
+        default: '/'
+      },
+      connection: {
+        secure: {
+          doc: 'RabbitMQ secure protocol',
+          format: Boolean,
+          env: 'RABBITMQ_SECURE',
+          default: false
+        },
+        hostname: {
+          doc: 'RabbitMQ hostname',
+          format: String,
+          env: 'RABBITMQ_HOSTNAME',
+          default: 'localhost'
+        },
+        port: {
+          doc: 'RabbitMQ amqp port',
+          format: Number,
+          env: 'RABBITMQ_PORT',
+          default: 5672
+        }
+      }
+    },
+    exchangeSettings: {
+      name: {
+        doc: 'RabbitMQ exchange name',
+        format: String,
+        env: 'RABBITMQ_EXCHANGE_NAME',
+        default: 'domain_events'
+      }
+    },
+    maxRetries: {
+      doc: 'Max number of retries for each message',
+      format: Number,
+      env: 'RABBITMQ_MAX_RETRIES',
+      default: 3
+    },
+    retryTtl: {
+      doc: 'Ttl for messages in the retry queue',
+      format: Number,
+      env: 'RABBITMQ_RETRY_TTL',
+      default: 1000
+    }
+  },  
+  pubsub:{
+    topic: {
+      doc: 'GCP PubSub topic name',
+      format: String,
+      env: 'PUBSUB_TOPIC_NAME',
+      default: 'projects/gc-k-mex-amex-b2b-dev-2/topics/domain_events'
+    },
+    subscription: {
+      doc: 'GCP PubSub subscription name',
+      format: String,
+      env: 'PUBSUB_SUBSCRIPTION_NAME',
+      default: 'projects/gc-k-mex-amex-b2b-dev-2/subscriptions/microservice1'
+    }
+  },
+});
+
+customerConfig.loadFile([__dirname + '/default.json', __dirname + '/' + customerConfig.get('env') + '.json']);
+
+export default customerConfig;
